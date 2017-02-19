a0ce814e41cd492d244f2165014f4f30bf1b1f42
Peter Grman <peter.grman@gmail.com>
2017-02-19 23:46:47 +0100

    Try custom docker image to generate proxy

--- update.ps1
+++ update.ps1
@@ -1,3 +1,3 @@
 $currentDir = (Get-Item -Path ".\" -Verbose).FullName
 
-docker run -it --rm -v "$($currentDir):/data" --add-host="localhost:10.0.75.1" apaleo/alpine-tooling /bin/bash /data/update.sh 
+docker run -it --rm -v "$($currentDir):/data" --add-host="localhost:10.0.75.1" apaleo/swagger-codegen /bin/bash /data/update.sh 
--- update.sh
+++ update.sh
@@ -7,23 +7,29 @@ dos2unix swaggers.txt
 gitBranch=$(basename $(git symbolic-ref HEAD))
 baseUrl="http://localhost:50000/"
 
-if [ "$gitBranch" == "master" ]; then
+#if [ "$gitBranch" == "master" ]; then
     baseUrl="https://ui-api.apaleo-staging.com/"
-fi
+#fi
 
 IFS='|'
 while read -r name url
 do
-    curl -k "$baseUrl$url" | \
-    jq -c '{spec: .}' | \
-    curl -k -H "Content-Type: application/json" -X POST -d @- https://generator.swagger.io/api/gen/clients/typescript-angular2 | \
-    jq '.link' | \
-    xargs curl -k > generated.zip
+    exec java -jar $GEN_DIR/modules/wagger-codegen-cli/target/swagger-codegen-cli.jar generate \
+        -i "$baseUrl$url" \
+        -t $GEN_DIR/modules/swagger-codegen/src/main/resources/typescript-angular2
+        -l typescript-angular2
+        -o ./src/$name
 
-    unzip generated.zip
+    # curl -k "$baseUrl$url" | \
+    # jq -c '{spec: .}' | \
+    # curl -k -H "Content-Type: application/json" -X POST -d @- https://generator.swagger.io/api/gen/clients/typescript-angular2 | \
+    # jq '.link' | \
+    # xargs curl -k > generated.zip
 
-    rm -rf "src/$name"
-    mv typescript-angular2-client "src/$name"
+    # unzip generated.zip
+
+    # rm -rf "src/$name"
+    # mv typescript-angular2-client "src/$name"
     echo "import * as _$name from './$name';" >> src/index.ts
     echo "export const $name = _$name;" >> src/index.ts
 
