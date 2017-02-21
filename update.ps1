2ebc1d65422b7d311f68b2d45879fbb13cff9c72
Peter Grman <peter.grman@gmail.com>
2017-02-21 19:14:03 +0100

    remove -it docker options

--- update.ps1
+++ update.ps1
@@ -4,4 +4,4 @@ param (
 
 $currentDir = (Get-Item -Path ".\" -Verbose).FullName
 
-docker run -it --rm -v "$($currentDir):/data" --add-host="localhost:10.0.75.1" apaleo/swagger-codegen /bin/bash /data/update_internal.sh $hostname
+docker run --rm -v "$($currentDir):/data" --add-host="localhost:10.0.75.1" apaleo/swagger-codegen /bin/bash /data/update_internal.sh $hostname
--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-docker run -it --rm -v "$CURRENT_DIR:/data" --add-host="localhost:10.0.75.1" apaleo/swagger-codegen $1
+docker run --rm -v "$CURRENT_DIR:/data" --add-host="localhost:10.0.75.1" apaleo/swagger-codegen $1
