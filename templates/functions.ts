d1a4bde146947688b54e454d462105b463423498
Peter Grman <peter.grman@gmail.com>
2017-03-20 01:40:39 +0100

    Improve form building options (#33)

---
+++ templates/functions.ts
@@ -0,0 +1,85 @@
+import { ValidatorFn, Validators, FormGroup } from '@angular/forms';
+import { IControlFactoryOptions, FormGroupControls, IBuildFormOptions, Control, Optional } from './types';
+
+export function getControl<T>(validators: ValidatorFn[], options?: IControlFactoryOptions<T>): [T | undefined, ValidatorFn] {
+    const finalValidators = [...validators];
+    let defaultValue: T | undefined = undefined;
+
+    if (options) {
+        defaultValue = options.defaultValue;
+
+        if (options.additionalValidators) {
+            finalValidators.push(...options.additionalValidators);
+        }
+    }
+    return [defaultValue, Validators.compose(finalValidators)];
+}
+
+export function getControlOptions<T, K extends keyof T>(options: IBuildFormOptions<T> | undefined, property: K) {
+    options = options || {};
+    let co: IControlFactoryOptions<T[K]> = {};
+
+    if (options.controlOptions) {
+        co = options.controlOptions[property] || {};
+    }
+    if (options.additionalValidators) {
+        const additionalValidators = options.additionalValidators[property] || [];
+
+        if (co.additionalValidators) {
+            co.additionalValidators.push(...additionalValidators);
+        } else {
+            co.additionalValidators = additionalValidators;
+        }
+    }
+    if (co.defaultValue === undefined && options.defaultValues) {
+        co.defaultValue = options.defaultValues[property];
+    }
+    return co;
+}
+
+export function adjustDefaultControls<T>(defaultControls: FormGroupControls<T>, options?: IBuildFormOptions<T>) {
+    if (!options) { return; }
+    
+    defaultControls = cleanUpDefaultControls(defaultControls, options);
+    return Object.assign(defaultControls, options.overwriteControls, convertAdditionalControls(options.additionalControls));
+}
+
+function cleanUpDefaultControls<T>(defaultControls: FormGroupControls<T>, options: IBuildFormOptions<T>) {
+    if (options.onlyInclude || options.skipControls) {
+        if (options.onlyInclude && options.skipControls) {
+            throw Error('You can either define "onlyInclude" or "skipControls" in IBuildFormOptions but never both.')
+        }
+        
+        if (options.onlyInclude) {
+            const keep = new Set<string>(options.onlyInclude);
+            options.skipControls = Object.keys(defaultControls).filter(k => !keep.has(k)) as (keyof T)[];
+        }
+
+        for (const key of options.skipControls!) {
+            delete defaultControls[key];
+        }
+    }
+    return defaultControls;
+}
+
+function convertAdditionalControls(additionalControls?: { [name: string]: (Control<any> | FormGroup | boolean) }) {
+    const ret: { [name: string]: (Control<any> | FormGroup) } = {};
+
+    if (!additionalControls) {
+        return ret;
+    }
+
+    for (const ctrl in additionalControls) {
+        if (additionalControls.hasOwnProperty(ctrl)) {
+            const ac = additionalControls[ctrl];
+
+            if (typeof ac === 'boolean') {
+                ret[ctrl] = [undefined, undefined];
+            } else {
+                ret[ctrl] = ac;
+            }
+        }
+    }
+
+    return ret;
+}
--- templates/types.ts
+++ templates/types.ts
@@ -1,8 +1,37 @@
-import { ValidatorFn } from '@angular/forms';
+import { ValidatorFn, FormGroup } from '@angular/forms';
 
-export type ValidatorsFactory = () => ValidatorFn[];
-export type Control = [null, ValidatorFn];
-export type ControlFactory = () => Control;
+export type Control<T> = [T | undefined, ValidatorFn | undefined];
+
+export interface IControlFactoryOptions<T> {
+    additionalValidators?: ValidatorFn[];
+    defaultValue?: T;
+}
+
+export type AdditionalValidators<T> = {
+    [P in keyof T]?: ValidatorFn[];
+}
+
+export type OverwriteControls<T> = {
+    [P in keyof T]?: Control<T[P]> | FormGroup
+}
+
+export type FormControlFactoryOptions<T> = {
+    [P in keyof T]?: IControlFactoryOptions<T[P]>
+}
+
+export interface IBuildFormOptions<T> {
+    overwriteControls?: OverwriteControls<T>;
+    additionalControls?: { [name: string]: (Control<any> | FormGroup | boolean) }
+    defaultValues?: Optional<T>;
+    additionalValidators?: AdditionalValidators<T>;
+    controlOptions?: FormControlFactoryOptions<T>;
+    skipControls?: (keyof T)[];
+    onlyInclude?: (keyof T)[];
+}
+
+export type FormGroupControls<T> = {
+    [P in keyof T]?: [T[P] | undefined, ValidatorFn] | FormGroup
+}
 
 export interface IApaleoAbstractControl {
     apaleoMetaData: IApaleoControlMetaData;
