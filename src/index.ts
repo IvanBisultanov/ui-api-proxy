e8e2f581b20c3c2837a30c5e293567c0d64b7f18
Benjamin Schmid <bennetone@users.noreply.github.com>
2017-06-20 17:22:47 +0200

    auto-update-c9df0d3 (#150)

--- src/index.ts
+++ src/index.ts
@@ -5,25 +5,25 @@ export * from './variables';
 
 import * as _account from './account';
 export const account = _account;
-export const accountModule: ApiModule = _account.Module;
+export const accountApiModule: ApiModule = _account.Module;
 
 import * as _inventory from './inventory';
 export const inventory = _inventory;
-export const inventoryModule: ApiModule = _account.Module;
+export const inventoryApiModule: ApiModule = _account.Module;
 
 import * as _rates from './rates';
 export const rates = _rates;
-export const ratesModule: ApiModule = _account.Module;
+export const ratesApiModule: ApiModule = _account.Module;
 
 import * as _booking from './booking';
 export const booking = _booking;
-export const bookingModule: ApiModule = _account.Module;
+export const bookingApiModule: ApiModule = _account.Module;
 
 import * as _settings from './settings';
 export const settings = _settings;
-export const settingsModule: ApiModule = _account.Module;
+export const settingsApiModule: ApiModule = _account.Module;
 
 import * as _finance from './finance';
 export const finance = _finance;
-export const financeModule: ApiModule = _account.Module;
+export const financeApiModule: ApiModule = _account.Module;
 
